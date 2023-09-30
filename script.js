e2a42755d1ea554684d67be4cf51a931262aeb62
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-09-30 16:53:26 +0500

    finally finished

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+function locomotiveAnimation(){
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+
+}
+locomotiveAnimation()
+
+function navAnimation(){
+
+    
+    gsap.to("#navpart1 svg",{
+        transform:"translateY(-100%)",
+        scrollTrigger:{
+            trigger:"#page1",
+        scroller:"#main",
+        start:"top 0",
+        end:"top -5%",
+        scrub:"true"
+    }
+})
+
+gsap.to("#navpart2 #link",{
+    transform:"translateY(-100%)",
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"#main",
+        start:"top 0",
+        end:"top -5%",
+        scrub:"true"
+    }
+})
+}
+navAnimation()
+
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
@@ -50,31 +113,55 @@ function loadingAnimation(){
 }
 loadingAnimation()
 
-document.addEventListener("mousemove",(dets)=>{
+
+const cursorAnimation = ()=>{
+
+
+    document.addEventListener("mousemove",(dets)=>{
         gsap.to("#cursor",{
             left:dets.x,
             top:dets.y
         })
-})
+    })
 
-document.querySelectorAll(".child").forEach((elem)=>{
-    elem.addEventListener("mouseenter",()=>{
-
-        gsap.to("#cursor",{
+    
+    document.querySelectorAll(".child").forEach((elem)=>{
+        elem.addEventListener("mouseenter",()=>{
             
+            gsap.to("#cursor",{
+                
                 transform: 'translate(-50%,-50%) scale(1) '
-            
+                
             })
         })
         elem.addEventListener("mouseleave",()=>{
-
+            
             gsap.to("#cursor",{
                 
-                    transform: 'translate(-50%,-50%) scale(0) '
+                transform: 'translate(-50%,-50%) scale(0) '
                 
-      })
+            })
             })
 })
+
+}
+cursorAnimation()
+
+
+
+var input  = document.getElementById("input")
+var icon  = document.getElementById("icon")
+
+
+input.addEventListener("focus",()=>{
+    icon.classList.remove("ri-arrow-right-line")
+    icon.classList.add("ri-corner-right-down-line")
+})
+input.addEventListener("blur",()=>{
+    icon.classList.remove("ri-corner-right-down-line")
+    icon.classList.add("ri-arrow-right-line")
+})
+
 
 
 
